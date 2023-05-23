c9365f9d7cb7df4cca9c4075e7406d19cb35400c
Alfsns <edgar_amanta_09@hotmail.com>
2023-05-23 10:34:23 -0500

    Tercera

--- src/main/java/com/aliencode/riomagicstore/controllers/ControllersBasic.java
+++ src/main/java/com/aliencode/riomagicstore/controllers/ControllersBasic.java
@@ -3,13 +3,20 @@ package com.aliencode.riomagicstore.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+
 
 @Controller
 
 public class ControllersBasic {
     @GetMapping("/saludar")
-    public String saludar(){
+    public String saludar(Model modelo){
         System.out.println("Hola mundo");
+        
+
+        //Consulta a la base de datos
+        String nombreUsuario ="Homero Simpson";
+        modelo.addAttribute("nombre", nombreUsuario);
         return "index";
     }
 }
